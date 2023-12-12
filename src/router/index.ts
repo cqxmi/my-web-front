86bb511210502d8e4bc620baec2cc41928cd2ad2
cqx <1175902886@qq.com>
2023-12-13 00:20:56 +0800

    图标、网页名、路由初步完成

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      //展示页
+      //展示
       path: '/',
       name: 'dashBoard',
       component: () => import('@/views/DashboardView.vue'),
@@ -42,7 +42,7 @@ const router = createRouter({
       ]
     },
     {
-      //登录页
+      //登录
       path: '/login',
       name: 'login',
       component: () => import('@/views/LoginView.vue')
