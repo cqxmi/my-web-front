939b1f6b1d84fd2f8c7c42ad03fcf0a189c58ed5
cqx <qinxu.chen@sumwhy.com>
2023-12-12 17:24:10 +0800

    初始化

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: import('@/views/HomeView.vue'),
+      children: [
+        // {
+        //   path: 'dashBoard',
+        //   component: UserProfile
+        // },
+        // {
+        //   path: 'posts',
+        //   component: UserPosts
+        // }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: import('@/views/LoginView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>start</div>
+</template>
+
+<style scoped></style>
