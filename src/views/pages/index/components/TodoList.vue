f8b4c7dff438adcebc6f8e7132cdf4c8ab495aa4
cqx <1175902886@qq.com>
2024-01-02 22:41:54 +0800

    三级tab完成

--- src/views/pages/index/components/TodoList.vue
+++ src/views/pages/index/components/TodoList.vue
@@ -8,13 +8,15 @@ const tab = ref(1)
 <template>
   <div class="todo-container">
     <div class="todo-icons">
-      <div class="icons-item" @click="tab = 1">
+      <div class="icons-item" :class="tab === 1 ? 'select' : ''" @click="tab = 1">
         <img v-if="tab === 1" class="icons" src="../../../../assets/menus/undo-select.png" />
         <img v-else class="icons" src="../../../../assets/menus/undo.png" />
+        <div class="icons-description">待完成</div>
       </div>
-      <div class="icons-item" @click="tab = 2">
+      <div class="icons-item" :class="tab === 2 ? 'select' : ''" @click="tab = 2">
         <img v-if="tab === 2" class="icons" src="../../../../assets/menus/done-select.png" />
         <img v-else class="icons" src="../../../../assets/menus/done.png" />
+        <div class="icons-description">已完成</div>
       </div>
     </div>
     <div class="todo-dashboard"></div>
@@ -23,37 +25,47 @@ const tab = ref(1)
 
 <style scoped lang="less">
 .todo-container {
-    width:100%;
-    height:100%;
+  width: 100%;
+  height: 100%;
   .todo-icons {
-    width: 5%;
+    width: 9%;
     border-right: 1px solid #f2f3f5;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-top: 18px;
-    height:100%;
-
+    padding-top: 6px;
+    height: 100%;
     .icons-item {
       padding: 5px;
       display: flex;
       justify-content: center;
       align-items: center;
-      border-radius: 50%;
-      margin-bottom: 18px;
+      margin-bottom: 3px;
       cursor: pointer;
-      border: 1px solid 9f9f9f;
+      border-radius: 6px;
 
       .icons {
-        width: 22px;
-        height: 22px;
+        width: 16px;
+        height: 16px;
+        margin-right: 6px;
       }
+      .icons-description {
+        font-size: 15px;
+        height: 21px;
+      }
+    }
+    .icons-item:hover {
+      background-color: #f0f0f0;
     }
   }
   .todo-dashboard {
     box-sizing: border-box;
     width: 100%;
     height: 100%;
+  }
+  .select {
+    background-color: #e9f7ee !important;
+    color:#42b883 !important;
   }
 }
 </style>
