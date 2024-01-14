b78a09987254ae4d940b5e7f6723559c517c37c5
cqx <1175902886@qq.com>
2024-01-14 13:56:06 +0800

    开始设计图片

--- src/views/pages/picture/PictureView.vue
+++ src/views/pages/picture/PictureView.vue
@@ -1,7 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>picture</div>
+  <div class="main-box">
+    <div class="main-list">
+      <div class="pic-item">
+        <div class="pic-icon"></div>
+      </div>
+      <div class="pic-item">
+        <div class="pic-icon"></div>
+      </div>
+      <div class="pic-item">
+        <div class="pic-icon"></div>
+      </div>
+      <div class="pic-item">
+        <div class="pic-icon"></div>
+      </div>
+      <div class="pic-item">
+        <div class="pic-icon"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="less">
+.main-box {
+  .main-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .pic-item {
+      margin-left:16px;
+      .pic-icon {
+        width: 300px;
+        height: 135px;
+        background-color: pink;
+      }
+    }
+  }
+
+}
+</style>
